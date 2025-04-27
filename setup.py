565247a98bfe33698052fc16505ab137476a389f
Samuel Longaer <samuel.longauer@gmail.com>
2025-04-27 07:13:28 +0200

    refresh_view

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read_requirements():
 
 setup(
     name="task-scheduler-x",
-    version="1.0.21",
+    version="1.0.22",
     author="Samuel Longauer",
     author_email="samuel.longauer@gmail.com",
     description="A CLI-based Task Scheduling Application",
